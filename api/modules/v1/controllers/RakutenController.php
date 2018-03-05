43251fc6a5ffdb0e1e3fdd906fc89417093170e5
angelo@sportspass.com.au <angelo@sportspass.com.au>
2018-03-05 16:47:16 +0800

    Rakuten Function Definition

--- api/modules/v1/controllers/RakutenController.php
+++ api/modules/v1/controllers/RakutenController.php
@@ -2,7 +2,7 @@
 
 namespace app\api\modules\v1\controllers;
 
-
+use Carbon\Carbon;
 use yii\base\Module;
 use Affiliate\Rakuten;
 use SimpleXMLElement;
@@ -119,9 +119,19 @@ class RakutenController extends Controller
         $this->accessToken = $token;
     }
 
+    /**
+     * Get All Approved Merchant / Retailer
+     *
+     * @desc Save in Active Record.
+     */
     public function actionMerchantByAppStatus()
     {
         $data = $this->merchantByAppStatus('approved');
+
+        //@todo save retailer data in DB
+        //@todo save offer / commission
+        //@todo save categories and match
+        //@todo get banners links
 
         if ($data) {
             pr($data);
@@ -149,8 +159,6 @@ class RakutenController extends Controller
      */
     public function merchantByAppStatus($status)
     {
-        $response = [];
-
         $header[] = 'Authorization: '.self::HEADER_TYPE_BEARER. ' '. $this->getToken();
         $link  = Rakuten::BASE_API_URL.'/'.self::API_NAME.'/'.self::API_VERSION.'/'.self::MERCHANT_BY_APP_STATUS.'/'.$status;
         $curl  = new Curl;
@@ -159,6 +167,93 @@ class RakutenController extends Controller
 
         $xmlElement = new SimpleXMLElement(XMLHelper::tidy($response));
         pr($xmlElement);
+    }
+
+    /**
+     * Allows you to download an advertiser’s information by specifying
+     * the LinkShare Advertiser ID for that advertiser.
+     *
+     * @param int $merchantId The LinkShare Advertiser ID
+     *
+     * @return $merchantId
+     */
+    public function merchantById($merchantId)
+    {
+        //@todo Implementation here
+
+        return $merchantId;
+    }
+
+    /**
+     * Allows you to download an advertiser’s information by specifying the name of the advertiser.
+     *
+     * @param string $name The name of the advertiser. It must be an exact match.
+     *
+     * @return $name
+     */
+    public function merchantByName($name)
+    {
+        //@todo Implementation here
+        return $name;
+    }
+
+    /**
+     * Allows you to download advertiser information by specifying the advertiser category.
+     *
+     * These are the same categories that you see when looking for advertisers in the
+     * Programs section of the Publisher Dashboard.
+     *
+     * @param int $categoryId The category of the advertiser
+     *
+     * @return $categoryId
+     */
+    public function merchantByCategory($categoryId)
+    {
+        //@todo Implementation here
+        return $categoryId;
+    }
+
+    /**
+     * Provides you the available banner links.
+     *
+     * To obtain specific banner links, you can filter this request using
+     * these parameters: MID, Category, Size, Start Date, and End Date.
+     *
+     * @param int         $merchantId This is the Rakuten LinkShare Advertiser ID.
+     *                                Optional, use -1 as the default value.
+     * @param int         $categoryId This is the Creative Category ID.
+     *                                It is assigned by the advertiser. Use the Creative Category
+     *                                feed to obtain it (not the Advertiser Category Table listed
+     *                                in the Publisher Help Center).
+     *                                Optional, use -1 as the default value.
+     * @param Carbon|null $startDate  This is the start date for the creative, formatted MMDDYYYY.
+     *                                Optional, use null as the default value.
+     * @param Carbon|null $endDate    This is the end date for the creative, formatted MMDDYYYY.
+     *                                Optional, use null as the default value.
+     * @param int         $size       This is the banner size code.
+     *                                Optional, use -1 as the default value.
+     * @param int         $campaignId Rakuten LinkShare retired this feature in August 2011.
+     *                                Please enter -1 as the default value.
+     * @param int         $page       This is the page number of the results.
+     *                                On queries with a large number of results, the system
+     *                                returns 10,000 results per page. This parameter helps
+     *                                you organize them.
+     *                                Optional, use 1 as a default value.
+     *
+     * @return $data[]
+     */
+    public function bannerLinks(
+        $merchantId = -1,
+        $categoryId = -1,
+        Carbon $startDate = null,
+        Carbon $endDate = null,
+        $size = -1,
+        $campaignId = -1,
+        $page = 1
+    ) {
+        $data = [];
+
+        return $data;
     }
 
     /**
